67238e9401c8989ac135cc257976dac64bb325c7
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2021-02-09 22:55:49 +0900

    ビンゴ制作2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
 'use strict';
 
 {
-  // const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  const source = [];
-  for (let i = 0; i < 15; i++) {
-    source[i] = i + 1;
+  function createColumn(col) {
+    const source = [];
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
+    }
+
+    const column = [];
+    for (let i = 0; i < 5; i++) {
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+
+    return column;
   }
 
-  const b = [];
-  // b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  for (let i = 0; i < 5; i++) {
-    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  }
+  const columns = [];
+  columns[0] = createColumn(0);
+  columns[1] = createColumn(1);
+  columns[2] = createColumn(2);
+  columns[3] = createColumn(3);
+  columns[4] = createColumn(4);
+  columns[2][2] = 'FREE';
 
-  console.log(b);
+  const bingo = [];
+  for (let row = 0; row < 5; row++) {
+    bingo[row] = [];
+    for (let col = 0; col < 5; col++) {
+      bingo[row][col] = columns[col][row];
+    }
+  }
+  console.table(bingo);
 }
